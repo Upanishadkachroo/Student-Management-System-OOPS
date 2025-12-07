390f857a18d8e71e9c424905d02df384c7e914f3
Upanishadkachroo <upanishadkachroo@gmail.com>
2025-12-07 18:28:12 +0530

    rest methods of get, put, remove done

--- HashMap/HashTable.java
+++ HashMap/HashTable.java
@@ -27,12 +27,55 @@ public class HashTable{
         return key%size;
     }
 
-    public void get(int key, int value){
-        
+    public int get(int key){
+        int idx=hashfunction(key);
+
+        for(Item item: table[idx]){
+            if(item.key==key){
+                return item.value;
+            }
+        }
+        //explicit throw
+        throw new RuntimeException("Key not found");
+    }
+    
+    public void remove(int key){
+        int idx=hashfunction(key);
+
+        for(int i=0; i<table[idx].size(); i++){
+            if(table[idx].get(i).key == key){
+                table[idx].remove(i);
+                return;
+            }
+        }
+        //runtime exception
+        throw new RuntimeException("Key not found");
     }
 
+    public void set(int key, int value){
+        int idx=hashfunction(key);
+
+        for(Item item: table[idx]){
+            if(item.key==key){
+                item.value=value;
+                return;
+                
+            }
+        }
+        table[idx].add(new Item(key, value));
+    }
 
     public static void main(String[] args) {
+        HashTable map = new HashTable(10);
 
+        map.set(1, 100);
+        map.set(11, 200); // collision -> same bucket
+        map.set(2, 300);
+
+        System.out.println(map.get(1));   // 100
+        System.out.println(map.get(11));  // 200
+
+        map.remove(1);
+        // System.out.println(map.get(1)); // would throw error
     }
 }
