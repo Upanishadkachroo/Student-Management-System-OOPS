1af45abb013c68910f1a52266379af4c22741235
Upanishadkachroo <upanishadkachroo@gmail.com>
2026-07-07 00:09:23 +0530

    implemented runtime polymorphism

--- revision/compiletimepolymorphism.cpp
+++ revision/compiletimepolymorphism.cpp
@@ -11,10 +11,55 @@ class Shape{
 class Circle: public Shape{
     public:
     void draw(){
-        
+        cout<<"Circle drawing.."<<endl;
     }
+};
+
+class Rectangle: public Shape{
+    public:
+    void draw(){
+        cout<<"Rectangle drawing.."<<endl;
+    }
+};
+
+class Triangle: public Shape{
+    public:
+    void draw() {
+        cout<<"Triangle drawing.."<<endl;
+    }
+};
+
+//making all the function of child class being accesed through its base class
+void shapedrawing(Shape *s){
+    s->draw(); //draw is polymorphic
 }
 
 int main(){
+    // Circle c;
+    // Rectangle r;
 
+    // // c.draw();
+    // // r.draw();
+
+    // shapedrawing(&c);
+    // shapedrawing(&r);
+
+    // Triangle *t=new Triangle();
+    // shapedrawing(t);
+
+    //w/o virtual keyword
+    Shape *s=new Shape();
+    s->draw();
+
+    //UPCASTING
+    Shape *s3=new Circle();
+    s3->draw();
+
+    Circle *c=new Circle();
+    c->draw();
+
+    //DOWNCASTING
+    Shape *s2=new Shape();
+    Circle *c2=(Circle *)s2;
+    c2->draw();
 }
